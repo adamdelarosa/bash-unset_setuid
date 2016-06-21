2f0421675b279e7121432b571ba3e2e43f7fab45
adamdelarosa <gojiradam@gmail.com>
2016-06-21 16:26:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bash-unset_setuid
---
+++ unset_setuid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+find . -user root -perm -4000 -exec ls  {} \; > setuid_Unset.txt
+
+for f in $(cat setuid_Unset.txt) ; do 
+  chmod ug-s $f
+done
+
